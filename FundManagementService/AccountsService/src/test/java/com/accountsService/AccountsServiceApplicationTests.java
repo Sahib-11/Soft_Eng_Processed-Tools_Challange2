536ea6e1844d166d4c5209588e2ccf5181ff3d58
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2022-10-11 05:07:45 +1100

    Added Initial App(Skeleton Code)

---
+++ FundManagementService/AccountsService/src/test/java/com/accountsService/AccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ FundManagementService/PersonsService/src/test/java/com/personsService/PersonsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
