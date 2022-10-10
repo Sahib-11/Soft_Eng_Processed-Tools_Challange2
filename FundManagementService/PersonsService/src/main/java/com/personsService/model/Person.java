98d345ba5f8bf85b99fe4ab170fde5980eb27a55
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2022-10-11 05:22:26 +1100

    Added Person.java class code in Model package

---
+++ FundManagementService/PersonsService/src/main/java/com/personsService/model/Person.java
@@ -0,0 +1,102 @@
+package com.personsService.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "postcode")
+    private int postcode;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "job")
+    private String job;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phoneno")
+    private int phoneno;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(int phoneno) {
+        this.phoneno = phoneno;
+    }
+}
