9821e4df94a929e08379781c95abfe5808117290
Sahibjeet Singh (s3842316) <s3842316@student.rmit.edu.au>
2022-10-11 22:08:00 +1100

    Added exceptions code

---
+++ FundManagementService/AccountsService/src/main/java/com/accountsService/exception/CustomExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.accountsService.exception;
+
+/**
+ * Credit homy1/Teachers
+ * https://github.com/Homy1/Teachers/tree/master/springbootrestdemo/src/main/java/com/homy/rest/exception
+ */
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings({"unchecked","rawtypes"})
+@ControllerAdvice
+public class CustomExceptionHandler
+{
+    @ExceptionHandler(ServletRequestBindingException.class)
+    public final ResponseEntity<Object> handleHeaderException(Exception ex, WebRequest request)
+    {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        com.accountsService.exception.ErrorResponse error = new com.accountsService.exception.ErrorResponse("Bad Request", details);
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request)
+    {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        com.accountsService.exception.ErrorResponse error = new com.accountsService.exception.ErrorResponse("Server Error", details);
+        return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
